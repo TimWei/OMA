376759af5157242d050fe1121e1b3506f992d913
timwei <aaasds2000@gmail.com>
2018-05-14 16:16:04 +0700

    change action desc in rex

--- oma_backend/app/controllers/api/v1/todo_list_item_controller.rb
+++ oma_backend/app/controllers/api/v1/todo_list_item_controller.rb
@@ -52,12 +52,12 @@ class Api::V1::TodoListItemController < AuthController
 		item.is_delete = params[:is_delete]
 		if item.save!
 			if params[:is_delete]
-				log = ActionLog.create(user: @user, todo_list: @list, action: 'delete', content: item.content, logable: item)
+				log = ActionLog.create(user: @user, todo_list: @list, action: 'deleted', content: item.content, logable: item)
 			else	
 				if params[:finished]
-					log = ActionLog.create(user: @user, todo_list: @list, action: 'finish', content: item.content, logable: item)
+					log = ActionLog.create(user: @user, todo_list: @list, action: 'finished', content: item.content, logable: item)
 				else
-					log = ActionLog.create(user: @user, todo_list: @list, action: 'recover', content: item.content, logable: item)
+					log = ActionLog.create(user: @user, todo_list: @list, action: 'undoned', content: item.content, logable: item)
 				end
 			end			
 			ActionCable.server.broadcast "list_#{@short_cut}",action: 'update_list_item', data: {
